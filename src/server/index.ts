85acd3c00d9020a97bbff8f41a57a49825319df4
Duckinm <duckii.mt@gmail.com>
2024-08-19 14:32:11 +0700

    chore: hide stuffs for now

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,19 +1,18 @@
-import { postController } from '@/server/routes/posts'
+// import { postController } from '@/server/routes/posts'
 import { treaty } from '@elysiajs/eden'
 import { swagger } from '@elysiajs/swagger'
 import { Elysia } from 'elysia'
 
-const app = new Elysia({ prefix: '/api' })
-  .use(swagger())
-  // .use(bearer())
-  // .onBeforeHandle(async ({ bearer, set }) => {
-  //   if (!bearer) return (set.status = 'Unauthorized')
-  //   const isAuthorized = bearer === '12345678'
-  //   if (!isAuthorized) {
-  //     return (set.status = 'Unauthorized')
-  //   }
-  // })
-  .use(postController)
+const app = new Elysia({ prefix: '/api' }).use(swagger())
+// .use(bearer())
+// .onBeforeHandle(async ({ bearer, set }) => {
+//   if (!bearer) return (set.status = 'Unauthorized')
+//   const isAuthorized = bearer === '12345678'
+//   if (!isAuthorized) {
+//     return (set.status = 'Unauthorized')
+//   }
+// })
+// .use(postController)
 
 export const client = treaty<App>(
   typeof window === 'undefined'
--- src/server/routes/posts/handlers.ts
+++ src/server/routes/posts/handlers.ts
@@ -1,117 +1,117 @@
-import type { Post } from './types'
+// import type { Post } from './types'
 
-export async function getPosts() {
-  try {
-    const files = Bun.file('./public/data.json')
-    const data = await files.json()
+// export async function getPosts() {
+//   try {
+//     const files = Bun.file('./public/data.json')
+//     const data = await files.json()
 
-    return data
-  } catch (error) {
-    console.log('Error getting posts:', error)
-  }
-}
+//     return data
+//   } catch (error) {
+//     console.log('Error getting posts:', error)
+//   }
+// }
 
-export async function getPostById(id: number) {
-  try {
-    const files = Bun.file('./public/data.json')
-    const data = (await files.json()) as {
-      data: Post[]
-    }
+// export async function getPostById(id: number) {
+//   try {
+//     const files = Bun.file('./public/data.json')
+//     const data = (await files.json()) as {
+//       data: Post[]
+//     }
 
-    return data.data.find((post) => post.id === id)
-  } catch (error) {
-    console.log('Error getting post by id:', error)
-  }
-}
+//     return data.data.find((post) => post.id === id)
+//   } catch (error) {
+//     console.log('Error getting post by id:', error)
+//   }
+// }
 
-export async function createPost({
-  title,
-  content,
-}: {
-  title: string
-  content: string
-}) {
-  try {
-    const files = Bun.file('./public/data.json')
-    const data = (await files.json()) as {
-      data: Post[]
-    }
+// export async function createPost({
+//   title,
+//   content,
+// }: {
+//   title: string
+//   content: string
+// }) {
+//   try {
+//     const files = Bun.file('./public/data.json')
+//     const data = (await files.json()) as {
+//       data: Post[]
+//     }
 
-    // if no data, pust it, else append it
-    if (!data) {
-      await Bun.write(
-        './public/data.json',
-        JSON.stringify({
-          data: [
-            {
-              id: 1,
-              title,
-              content,
-            },
-          ],
-        })
-      )
-      return
-    }
+//     // if no data, pust it, else append it
+//     if (!data) {
+//       await Bun.write(
+//         './public/data.json',
+//         JSON.stringify({
+//           data: [
+//             {
+//               id: 1,
+//               title,
+//               content,
+//             },
+//           ],
+//         })
+//       )
+//       return
+//     }
 
-    const newPost = {
-      id: data.data.length + 1,
-      title: 'New Post',
-      content: 'This is a new post',
-    }
+//     const newPost = {
+//       id: data.data.length + 1,
+//       title: 'New Post',
+//       content: 'This is a new post',
+//     }
 
-    data.data.push(newPost)
-    await Bun.write('./public/data.json', JSON.stringify(data))
-  } catch (error) {
-    console.log('Error creating post:', error)
-  }
-}
+//     data.data.push(newPost)
+//     await Bun.write('./public/data.json', JSON.stringify(data))
+//   } catch (error) {
+//     console.log('Error creating post:', error)
+//   }
+// }
 
-export async function updatePost(
-  id: number,
-  { title, content }: { title?: string; content?: string }
-) {
-  try {
-    const files = Bun.file('./public/data.json')
-    const data = (await files.json()) as {
-      data: Post[]
-    }
+// export async function updatePost(
+//   id: number,
+//   { title, content }: { title?: string; content?: string }
+// ) {
+//   try {
+//     const files = Bun.file('./public/data.json')
+//     const data = (await files.json()) as {
+//       data: Post[]
+//     }
 
-    const postIndex = data.data.findIndex((post) => post.id === id)
+//     const postIndex = data.data.findIndex((post) => post.id === id)
 
-    if (postIndex === -1) {
-      return
-    }
+//     if (postIndex === -1) {
+//       return
+//     }
 
-    data.data[postIndex] = {
-      id,
-      title: title ?? data.data[postIndex].title,
-      content: content ?? data.data[postIndex].content,
-    }
+//     data.data[postIndex] = {
+//       id,
+//       title: title ?? data.data[postIndex].title,
+//       content: content ?? data.data[postIndex].content,
+//     }
 
-    await Bun.write('./public/data.json', JSON.stringify(data))
-  } catch (error) {
-    console.log('Error updating post:', error)
-  }
-}
+//     await Bun.write('./public/data.json', JSON.stringify(data))
+//   } catch (error) {
+//     console.log('Error updating post:', error)
+//   }
+// }
 
-export async function deletePost(id: number) {
-  try {
-    const files = Bun.file('./public/data.json')
-    const data = (await files.json()) as {
-      data: Post[]
-    }
+// export async function deletePost(id: number) {
+//   try {
+//     const files = Bun.file('./public/data.json')
+//     const data = (await files.json()) as {
+//       data: Post[]
+//     }
 
-    const postIndex = data.data.findIndex((post) => post.id === id)
+//     const postIndex = data.data.findIndex((post) => post.id === id)
 
-    if (postIndex === -1) {
-      return
-    }
+//     if (postIndex === -1) {
+//       return
+//     }
 
-    data.data.splice(postIndex, 1)
+//     data.data.splice(postIndex, 1)
 
-    await Bun.write('./public/data.json', JSON.stringify(data))
-  } catch (error) {
-    console.log('Error deleting post:', error)
-  }
-}
+//     await Bun.write('./public/data.json', JSON.stringify(data))
+//   } catch (error) {
+//     console.log('Error deleting post:', error)
+//   }
+// }
